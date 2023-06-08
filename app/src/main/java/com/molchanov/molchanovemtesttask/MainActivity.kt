fe45bfdb5ea47c67fe575e63d739ded135e38349
lbuckl <ya_mvn@mail.ru>
2023-06-09 00:46:49 +0500

    MAIN: Добавлены основные элементы и логика для навигации MainActivity.kt

--- app/src/main/java/com/molchanov/molchanovemtesttask/MainActivity.kt
+++ app/src/main/java/com/molchanov/molchanovemtesttask/MainActivity.kt
@@ -11,10 +11,41 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun getViewBinding(): ActivityMainBinding {
-        TODO("Not yet implemented")
+        return ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun addMainFragment() {
-        TODO("Not yet implemented")
+        //TODO
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+        with(binding){
+            bnvMain.menu.let { menu ->
+                bnvMain.setOnItemSelectedListener { item ->
+                    when(item){
+                        menu.findItem(R.id.bv_item_general) -> {
+                            //TODO
+                        }
+                        menu.findItem(R.id.bv_item_search) -> {
+                            //TODO
+                        }
+                        menu.findItem(R.id.bv_item_basket) -> {
+                            //TODO
+                        }
+                        menu.findItem(R.id.bv_item_account) -> {
+                            //TODO
+                        }
+                    }
+                    true
+                }
+            }
+        }
+
     }
 }
